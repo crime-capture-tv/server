648baa4cc5401b711e30569391aa49ecfaba5dbf
ju <juunewon@gmail.com>
2023-09-11 20:25:58 +0900

    [Closes #9] Store entity column name 수정

--- src/main/java/com/mtvs/crimecapturetv/store/command/aggregate/entity/Store.java
+++ src/main/java/com/mtvs/crimecapturetv/store/command/aggregate/entity/Store.java
@@ -15,22 +15,22 @@ public class Store {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column(name = "store_no")
     private Long storeNo;
 
-    @Column
+    @Column(name = "store_name")
     private String storeName;
 
-    @Column
+    @Column(name = "store_address")
     private String StoreAddress;
 
     @Enumerated(EnumType.STRING)
     private StoreType storeType;
 
-    @Column
+    @Column(name = "business_regist_number")
     private String businessRegistNumber;
 
-    @Column
+    @Column(name = "store_phone_number")
     private String storePhoneNumber;
 
 }
