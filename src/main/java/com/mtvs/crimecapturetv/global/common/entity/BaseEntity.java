fd6a3db5fc19f497f398bbf0f2fb63c748565ae0
taegeun-park <taegeun0525@gamil.com>
2023-09-11 11:10:17 +0900

    [Feat] : BaseEntity 작성

---
+++ src/main/java/com/mtvs/crimecapturetv/global/common/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.mtvs.crimecapturetv.global.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
