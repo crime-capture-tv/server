9c10d85b1c18f94efc35212140a462f9c8ebb935
taegeun-park <taegeun0525@gamil.com>
2023-09-15 20:20:13 +0900

    [Feat] : CRIME_VIDEO_NOT_FOUND 에러코드 추가

--- src/main/java/com/mtvs/crimecapturetv/exception/ErrorCode.java
+++ src/main/java/com/mtvs/crimecapturetv/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     DUPLICATED_USER_ID(HttpStatus.CONFLICT, "User Id가 중복됩니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
     USER_NOT_FOUNDED(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
+    CRIME_VIDEO_NOT_FOUNDED(HttpStatus.NOT_FOUND, "해당 영상을 찾을 수 없습니다.")
     ;
 
     private HttpStatus status;
