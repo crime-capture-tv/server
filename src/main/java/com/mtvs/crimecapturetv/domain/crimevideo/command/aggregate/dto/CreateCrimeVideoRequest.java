77e4bcf518c47ccede050064de9bfa2227b7589f
taegeun-park <taegeun0525@gamil.com>
2023-09-17 14:53:44 +0900

    [BugFix] : 범죄영상 등록 request 필드 수정

--- src/main/java/com/mtvs/crimecapturetv/domain/crimevideo/command/aggregate/dto/CreateCrimeVideoRequest.java
+++ src/main/java/com/mtvs/crimecapturetv/domain/crimevideo/command/aggregate/dto/CreateCrimeVideoRequest.java
@@ -5,14 +5,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Builder
 public class CreateCrimeVideoRequest {
 
-    private LocalDateTime recordedAt;
     private String suspicionVideoPath;
 }
