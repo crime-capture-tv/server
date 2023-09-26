b538b7bff0a5d37a2a0c72b4d9139e180f4ff608
ju <juunewon@gmail.com>
2023-09-26 18:01:57 +0900

    게시판 양식 수정

--- src/main/java/com/mtvs/crimecapturetv/store/command/controller/exController.java
+++ src/main/java/com/mtvs/crimecapturetv/store/command/controller/exController.java
@@ -1,5 +1,6 @@
 package com.mtvs.crimecapturetv.store.command.controller;
 
+import com.mtvs.crimecapturetv.store.command.aggregate.dto.request.CommandStoreCreateRequest;
 import com.mtvs.crimecapturetv.store.command.service.CommandStoreService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,19 @@ public class exController {
 
     @GetMapping("/store-regist")
     public String store() { return "stores/store-regist";}
+
+    @PostMapping("/store-regist")
+    public String registStore(@ModelAttribute CommandStoreCreateRequest commandStoreCreateRequest, Long id) {
+
+        commandStoreService.registStore(commandStoreCreateRequest, id);
+
+        return "redirect:/stores/store-list";
+    }
+
+    @GetMapping("/store-list")
+    public String storeList() { return "stores/store-list";}
+
+
 
 
 
